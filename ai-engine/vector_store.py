5c0fcf325430e6f5b8cd5f8485901951b1cd2fdd
Arif Akbar <arifakbarkv1@gmail.com>
2026-04-18 22:07:34 +0530

    Build Vector Index

--- ai-engine/vector_store.py
+++ ai-engine/vector_store.py
@@ -1,23 +1,37 @@
-import os 
+import os
 
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 
 from embedder import chunk_documents
 
-DB_NAME = 'vector_db'
-EMBEDDER_MODEL_NAME = 'sentence-transformers/all-MiniLM-L6-v2'
+DB_NAME = "vector_db"
+EMBEDDER_MODEL_NAME = "sentence-transformers/all-MiniLM-L6-v2"
 
 def create_vectorstore():
     try:
         chunks = chunk_documents()
-        if len(chunks) > 0:
-            embeddings = HuggingFaceEmbeddings(model_name=EMBEDDER_MODEL_NAME)
-            if os.path.exists(DB_NAME):
-                Chroma(persist_directory=DB_NAME, embedding_function=embeddings).delete_collection()
-            vectore_store = Chroma.from_documents(chunks, embeddings, persist_directory=DB_NAME)
-            print ("Vector Store Created")
-        return vectore_store 
+
+        if not chunks:
+            print("No documents found. Skipping vector store creation.")
+            return None
+
+        embeddings = HuggingFaceEmbeddings(model_name=EMBEDDER_MODEL_NAME)
+
+        # In CI, usually no need to delete — fresh environment
+        vectore_store = Chroma.from_documents(
+            chunks,
+            embeddings,
+            persist_directory=DB_NAME
+        )
+
+        print("Vector Store Created Successfully")
+        return vectore_store
+
     except Exception as e:
         print("Error creating Vector Store:", e)
-        return []
+        return None
+
+
+if __name__ == "__main__":
+    create_vectorstore()
